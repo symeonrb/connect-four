1dfbc7faa48d4c97bbcfad910ecfb50b04b140c2
Romain Neil <tutosfaciles48@gmail.com>
2024-02-02 17:09:22 +0100

    feat: init project

---
+++ Connect Four/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Connect_four
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
