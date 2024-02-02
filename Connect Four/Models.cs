b4f7c03d48fca639d8aa002e3e949fe79b9b5693
Romain Neil <tutosfaciles48@gmail.com>
2024-02-02 18:47:40 +0100

    wip: base models context

---
+++ Connect Four/Models.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Connect_four
+{
+    
+    public class UserContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        private string DbPath { get; }
+
+        public UserContext()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            DbPath = System.IO.Path.Join(path, "users.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"Data Source={DbPath}");
+
+    }
+
+    public class User
+    {
+
+        public int UserId { get; set; }
+
+        public string UserUUID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserPassword { get; set; }
+
+    }
+
+}
