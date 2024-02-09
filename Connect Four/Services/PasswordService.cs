977812923c9613c02eb4d79500c8f39741b05f07
Romain Neil <tutosfaciles48@gmail.com>
2024-02-09 18:48:51 +0100

    feat: base password service

---
+++ Connect Four/Services/PasswordService.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Connect_four.Services;
+
+public class PasswordService
+{
+    private readonly HashAlgorithmName _hashAlgorithmName = HashAlgorithmName.SHA512;
+
+    private const int KeySize = 64;
+    private const int Iterations = 350000;
+
+    public string HashPassword(string clearPassword, out byte[] salt)
+    {
+        salt = RandomNumberGenerator.GetBytes(KeySize);
+
+        var hash = Rfc2898DeriveBytes.Pbkdf2(
+            Encoding.UTF8.GetBytes(clearPassword),
+            salt,
+            Iterations,
+            _hashAlgorithmName,
+            KeySize
+            );
+
+        return Convert.ToHexString(hash);
+    }
+    
+}
