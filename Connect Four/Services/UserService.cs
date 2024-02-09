8bde40c5d2694fab42447d1d01961118122a865f
Romain Neil <tutosfaciles48@gmail.com>
2024-02-09 15:48:02 +0100

    feat: base user service

---
+++ Connect Four/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿namespace Connect_four.Services;
+
+public class UserService
+{
+    private readonly UserContext _db = new UserContext();
+
+    /// <summary>
+    /// Créer un utilisateur
+    /// </summary>
+    /// <param name="username">Nom d'utilisateur</param>
+    /// <param name="password">Mot de passe</param>
+    public int Persist(string username, string password)
+    {
+        var user = new User
+        {
+            UserName = username,
+            UserPassword = password,
+            UserUUID = Guid.NewGuid().ToString()
+        };
+
+        _db.Add(user);
+        return _db.SaveChanges();
+    }
+    
+}
