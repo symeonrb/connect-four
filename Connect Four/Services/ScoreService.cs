314411d91720281ba3d9ff7e073267132831e438
Romain Neil <tutosfaciles48@gmail.com>
2024-02-09 15:48:52 +0100

    feat: base score service

---
+++ Connect Four/Services/ScoreService.cs
@@ -0,0 +1,21 @@
+﻿namespace Connect_four.Services;
+
+public class ScoreService
+{
+    
+    private readonly ScoreContext _db = new ScoreContext();
+
+    public int Persist(int userId, int score, bool againstAI = false)
+    {
+        var s = new Score
+        {
+            UserId = userId,
+            Result = score,
+            IsAgainstAI = againstAI
+        };
+        
+        _db.Add(s);
+        return _db.SaveChanges();
+    }
+    
+}
